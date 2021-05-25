a276ea77012ca55fa96741b4f7e1306b2912b859
KoR262 <wolf.artyom@gmail.com>
2021-05-25 17:17:00 +0500

    requirements

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     # extract your arg here
     print('Extracted arg is ==> %s' % sys.argv[2])
-    pytest.main([sys.argv[1]])
+    return(pytest.main([sys.argv[1]]))
 
 if __name__ == '__main__':
     main()
